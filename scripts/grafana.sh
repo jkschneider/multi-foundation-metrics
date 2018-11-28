cf809f6dc72b7bb66c4dc3ceda2921a7465b4a68
Jon Schneider <jschneider@pivotal.io>
2018-11-28 07:57:45 -0600

    Stop shipping to pushgateway

--- scripts/grafana.sh
+++ scripts/grafana.sh
@@ -2,8 +2,6 @@
 docker run -i -p 3000:3000 \
 -v $(pwd)/grafana-datasource.yml:/etc/grafana/provisioning/datasources/grafana-datasource.yml \
 -v $(pwd)/grafana-dashboard.yml:/etc/grafana/provisioning/dashboards/grafana-dashboard.yml \
--v $(pwd)/jvmgc-dashboard.json:/etc/grafana/dashboards/jvmgc.json \
--v $(pwd)/latency-dashboard.json:/etc/grafana/dashboards/latency.json \
--v $(pwd)/processor-dashboard.json:/etc/grafana/dashboards/processor.json \
+-v $(pwd)/by-foundation.json:/etc/grafana/dashboards/jvmgc.json \
+-v $(pwd)/by-instance.json:/etc/grafana/dashboards/latency.json \
 grafana/grafana:5.1.0
-
