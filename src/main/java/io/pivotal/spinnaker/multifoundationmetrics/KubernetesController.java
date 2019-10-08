16caa358d7b2c8ac822c7bc90df2e7ad68317210
Jon Schneider <jkschneider@gmail.com>
2019-10-08 12:31:35 -0500

    Log stuff

--- src/main/java/io/pivotal/spinnaker/multifoundationmetrics/KubernetesController.java
+++ src/main/java/io/pivotal/spinnaker/multifoundationmetrics/KubernetesController.java
@@ -1,5 +1,6 @@
 package io.pivotal.spinnaker.multifoundationmetrics;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.kubernetes.PodUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,9 @@ public class KubernetesController {
 
   public KubernetesController(PodUtils podUtils) {
     this.podUtils = podUtils;
+    for (Map.Entry<String, String> annot : podUtils.currentPod().get().getMetadata().getAnnotations().entrySet()) {
+      LoggerFactory.getLogger("k8s").warn(annot.getKey() + "=" + annot.getValue());
+    }
   }
 
   @GetMapping("/k8s/annotations")
