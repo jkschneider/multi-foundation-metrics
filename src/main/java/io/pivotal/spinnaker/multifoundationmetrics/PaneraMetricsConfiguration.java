f91e0ccbc575aa831c055c6d797bcd0a7d14f1e2
Jon Schneider <jkschneider@gmail.com>
2019-10-08 17:38:07 -0500

    Add kubenertes exception handling

--- src/main/java/io/pivotal/spinnaker/multifoundationmetrics/PaneraMetricsConfiguration.java
+++ src/main/java/io/pivotal/spinnaker/multifoundationmetrics/PaneraMetricsConfiguration.java
@@ -1,47 +1,80 @@
 package io.pivotal.spinnaker.multifoundationmetrics;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
+import java.util.function.Function;
 
 import static java.util.Collections.emptyMap;
 
 @Configuration
-@ConditionalOnBean(KubernetesClient.class)
 public class PaneraMetricsConfiguration {
-  @Bean
-  MeterFilter kubernetesMeterFilter(KubernetesClient k8sClient,
-                                    @Value("${spring.application.name:unknown}") String appName) {
-    Map<String, String> annotations = k8sClient.pods()
-      .withName(System.getenv("HOSTNAME"))
-      .get()
-      .getMetadata()
-      .getOwnerReferences()
-      .stream()
-      .findFirst()
-      .map(ref -> k8sClient.apps().replicaSets().withName(ref.getName()).get())
-      .map(replicaSet -> replicaSet.getMetadata().getAnnotations())
-      .orElse(emptyMap());
+  private final Logger logger = LoggerFactory.getLogger(PaneraMetricsConfiguration.class);
 
+  @Value("${spring.application.name:unknown}")
+  private String appName;
+
+  @Value("${HOSTNAME:unknown}")
+  private String host;
+
+  @Bean
+  @ConditionalOnBean(KubernetesClient.class)
+  MeterFilter kubernetesMeterFilter(KubernetesClient k8sClient) {
     return new MeterFilter() {
-      @Override
-      public Meter.Id map(Meter.Id id) {
-        return id.withTags(Tags.of(
-          "revision", annotations.getOrDefault("deployment.kubernetes.io/revision", "unknown"),
-          "application", annotations.getOrDefault("moniker.spinnaker.io/application", appName),
-          "cluster", annotations.getOrDefault("moniker.spinnaker.io/cluster", "unknown"),
-          "location", annotations.getOrDefault("artifact.spinnaker.io/location", "unknown")
-        ));
+      private Function<Meter.Id, Meter.Id> idMapper;
+
+      {
+        try {
+          Map<String, String> annotations = k8sClient.pods()
+            .withName(host)
+            .get()
+            .getMetadata()
+            .getOwnerReferences()
+            .stream()
+            .findFirst()
+            .map(ref -> k8sClient.apps().replicaSets().withName(ref.getName()).get())
+            .map(replicaSet -> replicaSet.getMetadata().getAnnotations())
+            .orElse(emptyMap());
+          idMapper = id -> id.withTags(Tags.of(
+            "revision", annotations.getOrDefault("deployment.kubernetes.io/revision", "unknown"),
+            "application", annotations.getOrDefault("moniker.spinnaker.io/application", appName),
+            "cluster", annotations.getOrDefault("moniker.spinnaker.io/cluster", "unknown"),
+            "location", annotations.getOrDefault("artifact.spinnaker.io/location", "unknown")
+          ));
+        } catch(KubernetesClientException e) {
+          logger.warn("Unable to apply kubernetes tags", e);
+          idMapper = id -> id.withTags(Tags.of("application", appName, "host", host));
+        }
       }
 
+      @Override
+      public Meter.Id map(Meter.Id id) {
+        return idMapper.apply(id);
+      }
+    };
+  }
+
+  @Bean
+  @ConditionalOnMissingBean(KubernetesClient.class)
+  MeterFilter appAndHostTagsMeterFilter() {
+    return MeterFilter.commonTags(Tags.of("application", appName, "host", host));
+  }
+
+  @Bean
+  MeterFilter httpDistributionStats() {
+    return new MeterFilter() {
       @Override
       public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
         if(id.getName().equals("http.server.requests")) {
